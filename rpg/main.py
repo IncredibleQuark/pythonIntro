c5c8aab0008249f8ce271bab330c37ddca192c09
IncredibleQuark <lkupinski@gmail.com>
2018-07-12 23:16:48 +0200

    combat init

--- rpg/main.py
+++ rpg/main.py
@@ -6,6 +6,26 @@ magic = [{"name": "Fire", "cost": 10, "dmg": 50},
 		 {"name": "Water Gun", "cost": 2, "dmg": 25}]
 
 player = Person(500, 80, 60, 35, magic)
+enemy = Person(800, 70, 45, 25, magic)
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+running = True
+
+print(BColors.FAIL + BColors.BOLD + "AN ENEMY ATTACKS!" + BColors.ENDC)
+
+while running:
+	print("================")
+	player.choose_action()
+	choice = input("Choose action:")
+	index = int(choice) - 1
+
+	if index == 0:
+		dmg = player.generate_damage()
+		enemy.take_damage(dmg)
+		print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+
+	enemy_choice = 1
+	enemy_dmg = enemy.generate_damage()
+	player.take_damage(enemy_dmg)
+	print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
+
+	# running = False
