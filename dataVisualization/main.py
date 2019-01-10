c75bada8b0210df365af42045006471ad72a58bb
IncredibleQuark <lkupinski@gmail.com>
2019-01-11 00:01:58 +0100

    styling

--- dataVisualization/main.py
+++ dataVisualization/main.py
@@ -4,7 +4,15 @@ years = [1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005,
 
 pops = [2.5, 2.7, 3.0, 3.3, 3.6, 4.0, 4.4, 4.8, 5.3, 5.7, 6.1, 6.5, 6.9, 7.3]
 
-plt.plot(years, pops, color=(255/255, 100/255, 100/255))
+deaths = [1.2, 1.7, 1.8, 2.0, 2.2, 2.2, 2.3, 2.1, 2.7, 3.0, 3.3, 3.0, 3.4, 3.5]
+
+lines = plt.plot(years, pops, years, deaths)
+
+# plt.plot(years, pops, color=(255/255, 100/255, 100/255))
+# plt.plot(years, deaths, '--', color=(0, 0, 0))
+plt.grid(True)
+plt.setp(lines, color=(1, .4, .4), marker="o")
+
 
 plt.ylabel("World population in billions")
 plt.xlabel("Year")
